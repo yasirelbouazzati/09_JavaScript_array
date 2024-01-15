d82360509c68f6a7bf30cb01454bd0012205281e
yasirelbouazzati <yasir12bouazzati@gmail.com>
2024-01-15 18:41:39 +0100

    activity0 complete

--- 01-map/capitalizeMates.js
+++
@@ -1,7 +0,0 @@
-/**
- * Given MATES array, define function 'capitalizeMates' to return an array of strings with capitalized names
- *
- * expected: ["John", "Jacob", "Jingleheimer", "Schmidt"]
- */
-
-const MATES = ["john", "JACOB", "jinGleHeimer", "schmidt"];
--- 01-map/feedMonkeys.js
+++
@@ -1,9 +0,0 @@
-/**
- * Given 'MONKEYS' array, define function 'feedMonkeys' to return an array where each monkey has one fruit
- * use for() instruction
- *
- * expected: with 🍌 => ['🐒 🍌', '🦍 🍌', '🦧 🍌']
- * expected: with 🍎 => ['🐒 🍎', '🦍 🍎', '🦧 🍎']
- */
-
-const MONKEYS = ["🐒", "🦍", "🦧"];
